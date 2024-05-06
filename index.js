fb3794dd986e672320b92d48270a24cfaa335e38
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2024-05-06 11:16:34 +0530

    added origin

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const path = require("path");
 // app.use(express.static(path.join(__dirname,"/public/Images")));
 app.use(express.static("public/Images"));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  // origin:"http://localhost:3000"
+   origin :"https://tapop-assaign-ment-frontend.vercel.app"
+}));
 app.use(bodyParser.json())
 // app.use( bodyParser.urlencoded({extended: true }));
 app.use(fileUpload({
