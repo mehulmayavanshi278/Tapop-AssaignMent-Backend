3dcdc21d01d1f1e739d8e75cefe9d2c323904a7b
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2024-05-06 11:39:28 +0530

    remp

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const path = require("path");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors({
-  origin:"http://localhost:3000"
-  //  origin :"https://tapop-assaign-ment-frontend.vercel.app"
+  // origin:"http://localhost:3000"
+   origin :"https://tapop-assaign-ment-frontend.vercel.app"
 }));
 app.use(bodyParser.json())
 // app.use( bodyParser.urlencoded({extended: true }));
