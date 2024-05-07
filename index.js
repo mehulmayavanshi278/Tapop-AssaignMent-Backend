f1eeba69a4db315e122b5a3c7fdaf12ec3f806a5
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2024-05-08 00:13:21 +0530

    tmp

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ const path = require("path");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors({
-  origin: "https://tapop-assaign-ment-frontend.vercel.app",
+  // origin: "https://tapop-assaign-ment-frontend.vercel.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed request headers
-  credentials: true,
-  preflightContinue: true,
-  optionsSuccessStatus: 204,
+  // credentials: true,
+  // preflightContinue: true,
+  // optionsSuccessStatus: 204,
 }));
 
 // app.use(bodyParser.json())
