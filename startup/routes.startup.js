93fa23f95e7a16ab0fedb2f5da64c87c4f2fc32e
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2024-05-08 02:24:42 +0530

    ad to inside function

--- startup/routes.startup.js
+++ startup/routes.startup.js
@@ -1,10 +1,12 @@
 
-const express = require("express");
-const { userRouter } = require("../routes/user.routes");
+
 
 
 
 module.exports = (app)=>{
+
+const express = require("express");
+const { userRouter } = require("../routes/user.routes");
   
     
     app.use("/user" , userRouter);
